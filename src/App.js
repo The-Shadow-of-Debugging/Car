0826c8ee64dbad7e4079106df7f2bab7d0712fad
Christina <c.parevskaya@afterlogic.com>
2022-11-30 16:17:51 +0300

    fixed bem

--- src/App.js
+++ src/App.js
@@ -1,94 +1,95 @@
-import './App.css';
+import './App.css'
 import {useEffect} from 'react'
 
 function App() {
   useEffect(() => {
-  const car = document.getElementById("car");
-  const speed = 10;
-  const coords = {
-    x: 0,
-    y: 0,
-  }
+    class Car {
 
-  function checkAngle(rotateDirection) {
-    switch(rotateDirection) {
-      case 'U':
-        rotateUp();
-        break;
-      case 'D':
-        rotateDown();
-        break;
-      case 'L':
-        rotateLeft();
-        break;
-      case 'R':
-        rotateRight();
-        break;
-    }
-  }
-
-  function rotateUp() {
-    car.style.transform = `rotate(0deg)`;
-  }
-
-  function rotateDown() {
-    car.style.transform = `rotate(180deg)`;
-  }
-
-  function rotateLeft() {
-    car.style.transform = `rotate(270deg)`;
-  }
-
-  function rotateRight() {
-    car.style.transform = `rotate(90deg)`;
-  }
-
-  function moveUp() {
-    checkAngle('U');
-    car.style.setProperty('top', coords.y - speed + 'px');
-    coords.y -= speed;
-  }
-
-  function moveDown() {
-    checkAngle('D');
-    car.style.setProperty('top', coords.y + speed + 'px');
-    coords.y += speed;
-  }
-
-  function moveLeft() {
-    checkAngle('L');
-    car.style.setProperty('left', coords.x - speed + 'px');
-    coords.x -= speed;
-  }
-
-  function moveRight() {
-    checkAngle('R');
-    car.style.setProperty('left', coords.x + speed + 'px');
-    coords.x += speed;
-  }
-
-  function checkPressAndExecute(key) {
-
-    if(key === "ArrowLeft") {
-      moveLeft();
     }
 
-    if(key === "ArrowRight") {
-      moveRight();
+    class Controller {
+
+    }
+    const car = document.getElementById('car')
+    const speed = 10
+    let previousStep = ''
+    let acceleration = 1
+    const coords = {
+      x: 0,
+      y: 0
     }
 
-    if(key === "ArrowUp") {
-      moveUp();
+    function checkAngle(rotateDirection) {
+      const directionHandler = {
+        'U': () => car.style.transform = 'rotate(0deg)',
+        'D': () => car.style.transform = 'rotate(180deg)',
+        'L': () => car.style.transform = 'rotate(270deg)',
+        'R': () => car.style.transform = 'rotate(90deg)',
+      }
+
+      directionHandler[rotateDirection]();
     }
 
-    if(key === "ArrowDown") {
-      moveDown();
+    function checkPreviousStep(currentStep) {
+      if (previousStep === currentStep) {
+        acceleration += 1
+      } else {
+        acceleration = 1
+      }
     }
-  }
 
-  document.addEventListener("keydown", (e) => {
-    checkPressAndExecute(e.key)
-  })
+    //todo: rework and union move methods
+    //todo: to limit car position by window bounds
+    // todo: движение машины по стрелкам с адекватным углом поворота
+    // todo: вынести в классы
+
+    function moveUp() {
+      checkAngle('U')
+      checkPreviousStep('U')
+      car.style.setProperty('top', coords.y - speed * acceleration + 'px')
+      coords.y -= speed * acceleration
+      previousStep = 'U'
+    }
+
+    function moveDown() {
+      checkAngle('D')
+      checkPreviousStep('D')
+      car.style.setProperty('top', coords.y + speed * acceleration + 'px')
+      coords.y += speed * acceleration
+      previousStep = 'D'
+    }
+
+    function moveLeft() {
+      checkAngle('L')
+      checkPreviousStep('L')
+      car.style.setProperty('left', coords.x - speed * acceleration + 'px')
+      coords.x -= speed * acceleration
+      previousStep = 'L'
+    }
+
+    function moveRight() {
+      checkAngle('R')
+      checkPreviousStep('R')
+      car.style.setProperty('left', coords.x + speed * acceleration + 'px')
+      coords.x += speed * acceleration
+      previousStep = 'R'
+    }
+
+    function checkPressAndExecute(key) {
+
+      const objectHandler = {
+        'ArrowLeft': moveLeft,
+        'ArrowRight': moveRight,
+        'ArrowUp': moveUp,
+        'ArrowDown': moveDown,
+      }
+
+      objectHandler[key]()
+    }
+
+    document.addEventListener('keydown', (e) => {
+      checkPressAndExecute(e.key)
+    })
 
   }, [])
 
@@ -97,12 +98,12 @@ function App() {
       <div className="car__body">
         <div className="car__shield"></div>
       </div>
-      <div className="car__front-left-wheel"></div>
-      <div className="car__front-right-wheel"></div>
-      <div className="car__back-left-wheel"></div>
-      <div className="car__back-right-wheel"></div>
+      <div className="car__wheel car__wheel_front car_wheel_left"></div>
+      <div className="car__wheel car__wheel_front car_wheel_right"></div>
+      <div className="car__wheel car__wheel_back car_wheel_left"></div>
+      <div className="car__wheel car__wheel_back car_wheel_right"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
